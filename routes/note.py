4ecff744b86a7dcbbacfc600f57efa98374e5871
PrabalCode01 <prabals476@gmail.com>
2024-09-16 12:26:30 +0530

    refactoring

--- routes/note.py
+++ routes/note.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from models.note import Note
 from config.db import conn
 from schemas.note import noteEntity , notesEntity
@@ -10,19 +10,25 @@ note = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
-
-
 @note.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     docs = conn.notes.notes.find({})
     newDocs = []
     for doc in docs:
         newDocs.append({
-            "id": str(doc["_id"]),  # Convert ObjectId to string
-            "notes": doc.get("notes", "No Content")  # Safeguard against missing "notes"
+            "id": str(doc["_id"]),  
+            "notes": doc.get("desc", "No Content")  
         })
-    
-    # Print newDocs to the console for debugging
-    print(newDocs)
-    
     return templates.TemplateResponse("index.html", {"request": request, "newDocs": newDocs})
+    # return JSONResponse(content=newDocs)
+
+
+
+@note.post("/")
+async def create_item(request:Request):
+    form = await request.form()
+    formdict = dict(form)
+    formdict["important"] = True if formdict.get("important") =="on" else False
+    note = conn.notes.notes.insert_one(formdict)
+    return {"Success":True}
+    
